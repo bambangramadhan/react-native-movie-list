b3a64bf1fa18db174b9f21a5221bab15cce5afd5
bambangramadhan <bambangramadhan96@gmail.com>
2024-05-16 18:03:37 +0700

    add custom fonts & colors

---
+++ src/theme/Font.tsx
@@ -0,0 +1,14 @@
+const Font = {
+  NotoBlack: 'NotoSerif-Black',
+  NotoBold: 'NotoSerif-Bold',
+  NotoExtraBold: 'NotoSerif-ExtraBold',
+  NotoExtraLight: 'NotoSerif-ExtraLight',
+  NotoItalic: 'NotoSerif-Italic',
+  NotoLight: 'NotoSerif-Light',
+  NotoMedium: 'NotoSerif-Medium',
+  NotoRegular: 'NotoSerif-Regular',
+  NotoSemiBold: 'NotoSerif-SemiBold',
+  NotoThin: 'NotoSerif-Thin',
+};
+
+export default Font;
